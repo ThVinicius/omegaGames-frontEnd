48c9b80bcafd4d3d5072bd6e3570df89cfa73395
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-07-06 23:12:59 -0300

    route Home incomplete

--- src/components/home/main/HomeScreen.js
+++ src/components/home/main/HomeScreen.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react'
+import { get } from 'axios'
 import { Container, Content, Header } from './styles'
 
 export default function HomeScreen() {
+  const [games, setGames] = useState()
+
+  useEffect(() => {
+    const URL = process.env.REACT_APP_API_URL
+
+    const promise = get(`${URL}/games`)
+
+    promise.then(({ data }) => {
+      setGames(data)
+    })
+  }, [])
+
   return (
     <Container>
       <Content>
